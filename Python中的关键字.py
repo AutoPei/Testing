a09232b5d8eb0f7f7015b57cf6ba11797d452a98
AutoPei <kalenzheng@gmail.com>
2025-10-04 21:32:08 +0800

    README.md

--- Python中的关键字.py
+++ Python中的关键字.py
@@ -1,3 +1,13 @@
 import keyword
 
-keyword.kwlist   #列出所有Python中的关键字
+key_word= ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif',
+ 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or',
+ 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+a = input("请输入关键词：")
+
+if a in key_word:
+    print("你输入的",a,"是关键词")
+else:
+    print("你输入的",a,"不是关键词。")
+    print("关键词列表如下：\n",key_word)
